c87d9f3b2d8fe54a0e4f87cabe9f24ba911d9a6e
Risam <risam1984@gmail.com>
2026-08-19 10:07:35 +0700

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ basePath: "/ovj",
+  assetPrefix: "/ovj",
+ allowedDevOrigins: ['192.168.188.100'],
 };
 
 export default nextConfig;
