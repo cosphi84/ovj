6ab29436b624d1c0ac012d70e8fe33097e1a9d14
Risam <risam1984@gmail.com>
2026-08-20 11:20:14 +0700

    fix api user

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -3,39 +3,49 @@ import { prisma } from "@/lib/db";
 import bcrypt from "bcryptjs";
 
 // get User by ID
-export async function GET(request: Request) {
-  try { 
-    const { searchParams } = new URL(request.url);
-    const id = searchParams.get("id");
-    if (!id) {  
-      return NextResponse.json(
-        { error: "User ID is required" },
-        { status: 400 }
-      );
+export async function GET(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    try {
+        const { id } = await params;
+        const userId = parseInt(id, 10);
+
+        if (isNaN(userId)) {
+            return NextResponse.json(
+                { error: "Invalid User ID" },
+                { status: 400 }
+            );
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                active: true,
+            },
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { error: "User not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json(user);
+    } catch (error) {
+        console.error("Failed to fetch user:", error);
+
+        return NextResponse.json(
+            { error: "Failed to fetch user" },
+            { status: 500 }
+        );
     }
-
-    const userId = parseInt(id, 10);
-
-    const user = await prisma.user.findUnique({
-      where: { id: userId, active: true },
-      select: { id: true, name: true, email: true },
-    });
-
-    if (!user) {
-      return NextResponse.json(
-        { error: "User not found" },
-        { status: 404 }
-      );
-    }
-
-    return NextResponse.json(user);
-  } catch (error) {
-    console.error("Failed to fetch user:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch user" },
-      { status: 500 }
-    );
-  }
 }
 
 export async function PATCH(
