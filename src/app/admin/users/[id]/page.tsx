4dd620181ed6fabff637cbf7248b2b69d47ceb63
Risam <risam1984@gmail.com>
2026-08-20 11:24:22 +0700

    fix user #1

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,18 +1,31 @@
-import EditUser from "@/components/admin/edituser"
-import { UserProfile } from "@/interface/user"
-import { ApiUrlUserEdit } from "@/schema/user"
-import { notFound } from "next/navigation"
+import EditUser from "@/components/admin/edituser";
+import { UserProfile } from "@/interface/user";
+import { ApiUrlUserEdit } from "@/schema/user";
+import { notFound } from "next/navigation";
 
-export default async function EditUserPage({ params }: {params : Promise<{id: string}>}){
-    const {id} = await params
-    const theId = Number(id)
-    
-    const user = await fetch(ApiUrlUserEdit(theId))
-    const usr: UserProfile = await user.json();
-    if (!user)
-    {
+export default async function EditUserPage({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+    const theId = Number(id);
+
+    if (isNaN(theId)) {
         notFound();
     }
 
-    return <EditUser user={usr} />
+    const response = await fetch(ApiUrlUserEdit(theId));
+
+    if (!response.ok) {
+        if (response.status === 404) {
+            notFound();
+        }
+
+        throw new Error("Failed to fetch user");
+    }
+
+    const usr: UserProfile = await response.json();
+
+    return <EditUser user={usr} />;
 }
